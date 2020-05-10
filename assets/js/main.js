a3ea0e04f6f72a85206b2e6aecc42722a6b05fb5
JordanFist <jordanyouk@neuf.fr>
2020-05-10 18:52:44 +0200

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,10 +178,9 @@
 	});
 
 	// Background
-	window.addEventListener('resize', () => {
 		// We execute the same script as before
 		let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	  });
+	  
 
 })(jQuery);
