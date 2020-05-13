71b070066aca63bab18e873ea22067b019a86de5
JordanFist <jordanyouk@neuf.fr>
2020-05-13 20:39:20 +0200

    test

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
+//import vh from '@sparing-software/100vh'
 (function($) {
-    //Loading height on touch-device
-        function calcFullHeight() {
-            jQuery('#bg').css("height", window.outerHeight);
-            //jQuery('#bg').css("height", $(window).height());
-        }
-    
-        (function($) {
-            calcFullHeight();
 
-            alert(window.outerHeight);
-            alert($(window).height());
-    
-            jQuery(window).on('orientationchange', function() {
-                // 500ms timeout for getting the correct height after orientation change
-                setTimeout(function() {
-                    calcFullHeight();
-                }, 500);
-    
-            });
-        })(jQuery);
-    
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
 })(jQuery);
