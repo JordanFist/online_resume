a34c13164b807552c32fc89793cb3a81b81fb6e5
JordanFist <jordanyouk@neuf.fr>
2020-05-13 20:06:17 +0200

    test

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 (function($) {
-    var viewportHeight = $('#bg').outerHeight();
-    console.log(viewportHeight);
-    $('#banner').css({ height: window.innerHeight });
-	  
+    //Loading height on touch-device
+        function calcFullHeight() {
+            jQuery('#bg').css("height", $(window).height());
+        }
+    
+        (function($) {
+            calcFullHeight();
+    
+            jQuery(window).on('orientationchange', function() {
+                // 500ms timeout for getting the correct height after orientation change
+                setTimeout(function() {
+                    calcFullHeight();
+                }, 500);
+    
+            });
+        })(jQuery);
+    
 })(jQuery);
